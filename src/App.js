3afb2410baf3eaa28d5184e637f582f0db17ab2d
MateuszWojtynski <wojtynski.mt@gmail.com>
2022-03-19 17:10:36 +0100

    Initial scheme of frontend-backend communication

--- src/App.js
+++ src/App.js
@@ -6,33 +6,60 @@ import LoginRegister from './components/Login/';
 import Ingredient from './components/Ingredient';
 import Recipes from './components/Recipes';
 import IngrRecSplit from './components/IngeRec-split';
+import { SessionContext, UserSession } from './session';
+import { useState } from 'react';
+import { deleteCookie, getCookie, setCookie } from './cookie';
 
 function App() {
+  const token = getCookie('login_token');
+  const [ session, setSession ] = useState(new UserSession(token));
+
+  const setNewSession = (newSession) => {
+    if(!newSession){
+      deleteCookie('login_token');
+    }
+    setSession(newSession);
+  }
+
   return (
-    <Router>
-      <div className="app">
-      <Navbar />
-      <div className="content">
-        
-          <Route path="/start">
-            <IngrRecSplit/>
-          </Route>
-         
-          <Route path="/login">
-            <LoginRegister/>
-          </Route>
-         
-          <Route path="/recipes">
-                <Recipes/>
-          </Route>
-          <Route path="/ingredients">
-                <Ingredient/>
-          </Route>
+    <SessionContext.Provider value = {{
+      session: session,
+      setSession: setNewSession
+    }}>
+      <Router>
+        <div className="app">
+        <Navbar />
+        <div className="content">
+          {
+            (
+              () => {
+                if(session && session.valid) {
+                  return [
+                    <Route path="/recipes">
+                      <Recipes/>
+                    </Route>,
+                    <Route path="/ingredients">
+                          <Ingredient/>
+                    </Route>,
+                    <Route path="/start">
+                      <IngrRecSplit/>
+                    </Route>
+                  ]
+                } else if (!session || !session.valid) {
+                  return [
+                    <Route path="/login">
+                      <LoginRegister/>
+                    </Route>
+                  ]
+                }
+              }
+            )()
+          }
+        </div>
+      
       </div>
-     
-    </div>
-    </Router>
-    
+      </Router>
+    </SessionContext.Provider>
     
   );
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import styles from './styles.css'
+import { useContext, useState } from 'react';
+import { apiUrl } from '../../api';
+import { setCookie } from '../../cookie';
+import { SessionContext, Session, UserSession } from '../../session';
 
 const switchers = [...document.querySelectorAll('.switcher')]
 
@@ -11,6 +16,63 @@ const switchers = [...document.querySelectorAll('.switcher')]
 })
 
 function LoginRegister() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [repPassword, setRepPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+
+  const {session, setSession} = useContext(SessionContext);
+  let navigate = useHistory();
+
+  function submitLogin(ev) {
+    ev.preventDefault();
+    fetch(`${apiUrl}/logging`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: username, password: password })
+    }).then(res => {
+      res.json().then((data) => {
+        if(data.error) {
+          setError(data.message);
+        } else {
+          setCookie('login_token', data.token, 1000 * 3600 * 2); //expires after 2 hours
+          setSession(new UserSession(data.token));
+          navigate.push('/start');
+        }
+      }).catch(error => {
+        console.error(error);
+        setError('Invalid server response');
+      });
+    }).catch(error => {
+      console.error(error);
+      setError('Failed to connect, try again later');
+    });
+  }
+
+  function submitRegister(ev) {
+    ev.preventDefault();
+    fetch(`${apiUrl}/registration`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: username, password: password, repPassword: repPassword, email: email })
+    }).then(res => {
+      res.json().then((data) => {
+        if(data.error) {
+          setError(data.message);
+        } else {
+          navigate.push('/login');
+        }
+      }).catch(error => {
+        console.error(error);
+        setError('Invalid server response');
+      });
+    }).catch(error => {
+      console.error(error);
+      setError('Failed to connect, try again later');
+    });
+  }
+
     
   return (
     <section className="forms-section">
@@ -20,16 +82,16 @@ function LoginRegister() {
         Login
         <span className="underline"></span>
       </button>
-      <form className="form form-login">
+      <form className="form form-login" action='#' onSubmit={submitLogin}>
         <fieldset>
           <legend>Please, enter your email and password for login.</legend>
           <div className="input-block">
-            <label htmlFor="login-email">E-mail</label>
-            <input id="login-email" type="email" required/>
+            <label htmlFor="login-username">Username</label>
+            <input id="login-username" type="text" required onInput={(ev) => { setUsername(ev.target.value); }}/>
           </div>
           <div className="input-block">
             <label htmlFor="login-password">Password</label>
-            <input id="login-password" type="password" required/>
+            <input id="login-password" type="password" required onInput={(ev) => { setPassword(ev.target.value); }}/>
           </div>
         </fieldset>
         <button type="submit" className="btn-login">Login</button>
@@ -40,23 +102,27 @@ function LoginRegister() {
         Sign Up
         <span className="underline"></span>
       </button>
-      <form className="form form-signup">
+      <form className="form form-signup" action='#' onSubmit={submitRegister}>
         <fieldset>
           <legend>Please, enter your email, password and password confirmation for sign up.</legend>
           <div className="input-block">
+            <label htmlFor="signup-username">Username</label>
+            <input id="signup-username" type="text" required onInput={(ev) => { setUsername(ev.target.value); }}/>
+          </div>
+          <div className="input-block">
             <label htmlFor="signup-email">E-mail</label>
-            <input id="signup-email" type="email" required/>
+            <input id="signup-email" type="email" required onInput={(ev) => { setEmail(ev.target.value); }}/>
           </div>
           <div className="input-block">
             <label htmlFor="signup-password">Password</label>
-            <input id="signup-password" type="password" required/>
+            <input id="signup-password" type="password" required onInput={(ev) => { setPassword(ev.target.value); }}/>
           </div>
           <div className="input-block">
             <label htmlFor="signup-password-confirm">Confirm password</label>
-            <input id="signup-password-confirm" type="password" required/>
+            <input id="signup-password-confirm" type="password" required onInput={(ev) => { setRepPassword(ev.target.value); }}/>
           </div>
         </fieldset>
-        <button type="submit" className="btn-signup">Continue</button>
+        <button type="submit" className="btn-signup">Register</button>
       </form>
     </div>
   </div>
