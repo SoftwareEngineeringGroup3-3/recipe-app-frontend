58f6f48eebe44307c79cc169ccd5ec577985e6f8
MateuszWojtynski <wojtynski.mt@gmail.com>
2022-03-20 23:02:59 +0100

    Code view redesign

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import LoginRegister from './components/Login/';
@@ -13,91 +13,71 @@ import { useState } from 'react';
 import { deleteCookie, getCookie, setCookie } from './cookie';
 
 function App() {
-    const token = getCookie('security_header');
-    const [session, setSession] = useState(new UserSession(token));
+  const token = getCookie('security_header');
+  const [ session, setSession ] = useState(new UserSession(token));
 
-    const setNewSession = (newSession) => {
-        if (!newSession) {
-            deleteCookie('security_header');
-        }
-        setSession(newSession);
+  const setNewSession = (newSession) => {
+    if(!newSession){
+      deleteCookie('security_header');
     }
+    setSession(newSession);
+  }
 
-    return ( <
-        SessionContext.Provider value = {
-            {
-                session: session,
-                setSession: setNewSession
-            }
-        } >
-        <
-        Router >
-        <
-        div className = "app" >
-
-
-
-        {
+  return (
+    <SessionContext.Provider value = {{
+      session: session,
+      setSession: setNewSession
+    }}>
+      <Router>
+        <div className="app">
+        
+          
+        
+          {
             (
-                () => {
-                    if (session && session.valid) {
-                        return [ <
-                            Route path = "/recipes" >
-                            <
-                            Navbar / >
-                            <
-                            Recipes / >
-                            <
-                            /Route>, <
-                            Route path = "/ingredients" >
-                            <
-                            Navbar / >
-                            <
-                            Ingredient / >
-                            <
-                            /Route>, <
-                            Route path = "/start" >
-                            <
-                            Navbar / >
-                            <
-                            IngrRecSplit / >
-                            <
-                            /Route>,
-                            // <Route path="/logStart">
-                            //   <Navbar/>
-                            //   <LogOutStart/>
-                            // </Route>,
-                        ]
-                    } else if (!session || !session.valid) {
-                        return [ <
-                            Route path = "/login" >
-                            <
-                            NavbarLogOut / >
-                            <
-                            LoginRegister / >
-                            <
-                            /Route>, <
-                            Route path = "/logStart" >
-                            <
-                            NavbarLogOut / >
-                            <
-                            LogOutStart / >
-                            <
-                            /Route>
-
-                        ]
-                    }
+              () => {
+                if(session && session.valid) {
+                  return [
+                   <Route path="/recipes">
+                      <Navbar/>
+                      <Recipes/>
+                    </Route>,
+                    <Route path="/ingredients">
+                      <Navbar/>
+                          <Ingredient/>
+                    </Route>,
+                    <Route path="/start">
+                      <Navbar/>
+                      <IngrRecSplit/>
+                    </Route>,
+                    // <Route path="/logStart">
+                    //   <Navbar/>
+                    //   <LogOutStart/>
+                    // </Route>,
+                    ]
+                } else if (!session || !session.valid) {
+                  return [
+                   <Route path="/login">
+                     <NavbarLogOut/>
+                    <LoginRegister/>
+                    </Route>,
+                    <Route path="/logStart">
+                      <NavbarLogOut/>
+                      <LogOutStart/>
+                    </Route>
+                  
+                  ]
                 }
+              }
             )()
-        } <
-        /div>
-
-
-        <
-        /Router> <
-        /SessionContext.Provider>
-
-    );
+          }
+        </div>
+      
+      
+      </Router>
+    </SessionContext.Provider>
+    
+  );
 }
 
 export default App;
