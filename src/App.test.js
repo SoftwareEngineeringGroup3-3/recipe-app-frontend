3c4d07f52c2097216867f01ca5c2eb50fb74710d
Jan Maliborski <janekmaliborski@gmail.com>
2022-04-06 00:49:41 +0200

    Fix test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('First test', () => {
+  expect(1+2).toEqual(3);
+  expect(2+2).toEqual(4);
 });
