dd303e3653e7674a516cabd89c546540571cd285
Bober-G <jakubwoch@gmail.com>
2022-04-12 15:23:43 +0200

    fixed styles for users

--- src/components/DisplayUsers/index.jsx
+++ src/components/DisplayUsers/index.jsx
@@ -3,31 +3,36 @@ import styles from './styles.css'
 import { useContext, useState } from 'react';
 import { apiUrl } from '../../api';
 import { useHistory } from 'react-router-dom';
+import editImg from '../../images/edit.png';
+import deleteImg from '../../images/delete.png';
 function DisplayUsers() {
     var users = ['user1', 'user2'];
-    var email = ['email1', 'email2'];
     return (
         <form className='user-disp'>
-
-            <table className="users" >
-                <tr className='user-rows'>
-                    <th>Username</th>
+            <table className="styled-table-users" >
+            <thead>
+                <tr>
+                    <th className='edit-first-column'>Name</th>
                     <th>Email</th>
-                    <th>Id</th>
+                    <th>ID</th>
                     <th>Admin</th>
+                    <th><img src={editImg} className="editImg" /></th>
+                    <th><img src={deleteImg} className="editImg" /></th>
                 </tr>
-                    {
-                        users.map((element,i) => <tr className='user-rows' key={i}>
-                            <td className='user-cols'>{element}</td>
-                            <td className='user-cols'>{element}</td>
-                            <td className='user-cols'>{element}</td>
-                            <td className='user-cols'>{element}</td>
-                            <button>Edit</button>
-                            <button>Delete</button>
-                        </tr>)
-                    }
-                
-                </table>
+            </thead>
+            <tbody>
+                {
+                    users.map((element, i) => <tr className='user-rows' key={i}>
+                        <td className='user-cols'>{element}</td>
+                        <td className='user-cols'>{element}</td>
+                        <td className='user-cols'>{element}</td>
+                        <td className='user-cols'>{element}</td>
+                        <td><button className="user-edit-button">Edit</button></td>
+                        <td><button className="DeleteButton" id="DeleteButton" type="submit" >Delete</button></td>
+                    </tr>)
+                }
+            </tbody>
+            </table>
         </form>
     )
 }
