2e0acad548227ee9b6bcf1fce275915c729cdc29
Jan Maliborski <janekmaliborski@gmail.com>
2022-05-18 17:22:05 +0200

    First Selenium test

---
+++ src/tests/AOpenPage.test.js
@@ -0,0 +1,18 @@
+const { Builder, By } = require('selenium-webdriver');
+
+it('Test 1 - Open Page for 5s', () => {
+    (async () => {
+        const driver = await new Builder().forBrowser('MicrosoftEdge').build();
+        try {
+            await driver.get('http://localhost:3000/');
+
+            driver.getTitle().then(function(title) {
+                expect("React App").toEqual(title);
+            });
+
+            await driver.sleep(5000);
+        } finally {
+            await driver.quit();
+        }
+    })();
+  });
